d8ca1ed6d08b83046bd2b6c7854ef0dc90034e64
Nikhil Chaudhary <Nikhilchaudhary9887@gmail.com>
2025-08-23 22:56:30 +0530

    memo done

--- 1-use-memo/src/components/Assignment3.jsx
+++ 1-use-memo/src/components/Assignment3.jsx
@@ -3,6 +3,11 @@ import React, { useState, useMemo } from 'react';
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
+        const [count , setCount] = useState(0)
+        const reRender = () =>{
+            setCount(count + 1)
+        }
+    
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
@@ -12,10 +17,20 @@ export const Assignment3 = () => {
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    let totalValue = 0;
+    totalValue = useMemo(()=>{
+        let res = 0;
+        items.map((x)=>{
+            res = x.value + res;
+        })
+        console.log(res);
+        
+        return res
+    },[items])
     // Your code ends here
     return (
         <div>
+             <button onClick={reRender}>Re-render {count}</button>
             <ul>
                 {items.map((item, index) => (
                     <li key={index}>{item.name} - Price: ${item.value}</li>
